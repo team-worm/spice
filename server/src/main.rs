46243e6d15392c3b72338f5ee1abb7d2a8ba381a
tarik <tarik.courdy@gmail.com>
2016-11-13 15:03:12 -0700

    REST endpoints using hyper and reroute

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,28 @@
+extern crate hyper;
+extern crate reroute;
+
+use hyper::server::{Server, Request, Response};
+use hyper::status::StatusCode;
+use reroute::{Captures, Router};
+
+fn basic_handler(req: Request, res: Response, c: Captures){
+    res.send(b"Hello World").unwrap();
+}
+
+fn filesystem_handler(req: Request, res: Response, c: Captures){
+    println!("captures: {:?}", c);
+    res.send(b"Here are the files at root").unwrap();
+}
+
 fn main() {
-    println!("Hello, world!");
+    //install routes
+    
+    let mut router = Router::new();
+    router.get("/greet", basic_handler);
+    router.get(r"/api/v1/filesystem/*", filesystem_handler);
+    
+    router.finalize().unwrap();
+
+    let server = Server::http("127.0.0.1:3000").unwrap();
+    server.handle(router).unwrap();
 }
