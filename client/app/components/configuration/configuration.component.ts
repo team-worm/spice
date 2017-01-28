f9639ad7888ec3fd444b79f70d79080224af1581
samdamana <samdamana@gmail.com>
2017-01-27 18:12:25 -0700

    Added grid layout template to configuration view

--- client/app/components/configuration/configuration.component.ts
+++ client/app/components/configuration/configuration.component.ts
@@ -2,6 +2,13 @@ import {Component} from "@angular/core";
 
 @Component({
     selector: 'spice-configuration',
-    template: `<h4>Config your stuff loser.</h4><button md-raised-button>Whatever, run the function.</button>`
+    template: `
+<h4>Config your stuff here like functions and types.</h4>
+<md-grid-list cols="3" rowHeight="170px">
+    <md-grid-tile [colspan]="1" [rowspan]="4" [style.background]="'lightpink'">Function Picker</md-grid-tile>
+    <md-grid-tile [colspan]="2" [rowspan]="1" [style.background]="'lightblue'">SourceType Stuff</md-grid-tile>
+    <md-grid-tile [colspan]="2" [rowspan]="3" [style.background]="'lightgreen'">Source Viewer</md-grid-tile>
+</md-grid-list>
+`
 })
 export class ConfigurationComponent {}
