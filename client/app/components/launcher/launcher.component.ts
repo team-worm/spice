70f8b54bcfdef4e792e9fcb3c794cfe11af999b7
samdamana <samdamana@gmail.com>
2017-01-31 19:49:06 -0700

    Added some basic snackbar error handling

--- client/app/components/launcher/launcher.component.ts
+++ client/app/components/launcher/launcher.component.ts
@@ -3,6 +3,8 @@ import {SourceFile} from "../../models/SourceFile";
 import {Process} from "../../models/Process";
 import {DebuggerService} from "../../services/debugger.service";
 import {MdSnackBar} from "@angular/material";
+import {DebuggerState} from "../../models/DebuggerState";
+import {Response} from "@angular/http";
 
 @Component({
     selector: 'spice-launcher',
@@ -28,9 +30,28 @@ export class LauncherComponent {
     }
 
     public LaunchBinary() {
+        if(this.selectedFile) {
+            let self = this;
+            let launchedFile = this.selectedFile;
+            this.debuggerService.attachBinary(this.selectedFile.path).subscribe({
+                next: (ds:DebuggerState)=>{console.log(ds)},
+                complete: ()=>{},
+                error: (error:Response)=>{
+                    self.snackBar.open('Error Launching '+launchedFile.name+' ('+error.status+'): '+error.statusText, undefined, {
+                        duration: 3000
+                    });
+                }
+            });
+        } else {
+            this.snackBar.open("No valid file selected to launch.", undefined, {
+                duration: 3000
+            });
+        }
 
     }
     public AttachToProcess() {
-
+        this.snackBar.open("Attaching to process not yet implemented.", undefined, {
+            duration: 1000
+        });
     }
 }
