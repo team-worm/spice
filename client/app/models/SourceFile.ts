3ef5882933a5fa0b30022036e9eae7e519d40641
samdamana <samdamana@gmail.com>
2017-02-07 15:56:09 -0700

    Implemented file-system http stuff and fixed up file-browser component.

--- client/app/models/SourceFile.ts
+++ client/app/models/SourceFile.ts
@@ -1,3 +1,4 @@
+import {SpiceValidator} from "../util/SpiceValidator";
 export class SourceFile {
     name: string; //Name of file.
     path: string; //Path to file.
@@ -6,4 +7,22 @@ export class SourceFile {
      * An array of Files inside the directory.
      * Subdirectories have "contents" field set to undefined so the entire directory subtree is not returned. */
     contents: SourceFile[] | undefined;
+
+    constructor(_name:string, _path:string, _fType:string, _contents:SourceFile[] | undefined) {
+        this.name = _name;
+        this.path = _path;
+        this.fType = _fType;
+        this.contents = _contents
+    }
+
+    static fromObjectStrict(obj: any): SourceFile {
+        SpiceValidator.assertTypeofStrict(obj, 'object');
+        SpiceValidator.assertTypeofStrict(obj.name, 'string');
+        SpiceValidator.assertTypeofStrict(obj.path, 'string');
+        SpiceValidator.assertTypeofStrict(obj.fType, 'string');
+        SpiceValidator.assertArrayStrict(obj.contents);
+
+        return new SourceFile(obj.name, obj.path, obj.fType, obj.contents);
+    }
+
 }
