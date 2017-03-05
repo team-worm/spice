b66d71f48d956c2cbb4b27d8e2b23d41265461f7
tcourdy <tarik.courdy@gmail.com>
2017-03-04 19:47:53 -0700

    Beginnings of UI polish -Convert graph var input to selector -Initially hide graph and reset it every time user runs the function

--- client/app/components/debugger/debugger.component.ts
+++ client/app/components/debugger/debugger.component.ts
@@ -15,156 +15,177 @@ import { Subscriber } from "rxjs/Subscriber";
 
 @Component({
     moduleId: module.id,
-	selector: 'spice-debugger',
-	templateUrl: './debugger.component.html'
+    selector: 'spice-debugger',
+    templateUrl: './debugger.component.html'
 })
 export class DebuggerComponent {
 
-	public lines: { sourceCode: string, traces: Trace[]}[];
-	public lastTraceLine: number;
-	public traceColCount: number;
+    public lines: { sourceCode: string, traces: Trace[] }[];
+    public lastTraceLine: number;
+    public traceColCount: number;
 
-	public sourceFunction: SourceFunction | null;
-	public debugState: DebuggerState | null;
-	public setParameters:{[id: string]: any};
+    public sourceFunction: SourceFunction | null;
+    public debugState: DebuggerState | null;
+    public setParameters: { [id: string]: any };
 
-	@ViewChild('lineGraph') lineGraph: LineGraphComponent;
-	public graphData: DataXY[] = [];
-	//public graphVariable: SourceVariable | null = null; //TODO: update this when we don't just use variable name in trace (and corresponding html)
-	public graphVariable: string = "";
-	public graphVariableName: string = "";
-	public currentExecution: ExecutionId | null = null;
+    @ViewChild('lineGraph') lineGraph: LineGraphComponent;
+    public graphData: DataXY[] = [];
+    //public graphVariable: SourceVariable | null = null; //TODO: update this when we don't just use variable name in trace (and corresponding html)
+    public graphVariable: string = "";
+    public graphVariableName: string = "";
+    public variables: Set<string>;
+    public showGraph: boolean;
 
-	constructor(private fileSystemService: FileSystemService,
-				private viewService: ViewService,
-				private snackBar: MdSnackBar) {
+    public currentExecution: ExecutionId | null = null;
 
-		this.viewService.debuggerComponent = this;
-		this.lastTraceLine = Number.POSITIVE_INFINITY;
-		this.traceColCount = 0;
-		this.lines = [];
-		this.sourceFunction = null;
-		this.setParameters = {};
-	}
+    constructor(private fileSystemService: FileSystemService,
+        private viewService: ViewService,
+        private snackBar: MdSnackBar) {
 
-	public displayTrace(executionId: ExecutionId) {
-		this.currentExecution = executionId;
-		if(this.debugState) {
-			let ds: DebuggerState = this.debugState;
-			if(this.graphVariable !== '') {
-				this.SetGraphVariable(this.graphVariable);
-			}
-			ds.getExecution(executionId)
+        this.viewService.debuggerComponent = this;
+        this.lastTraceLine = Number.POSITIVE_INFINITY;
+        this.traceColCount = 0;
+        this.lines = [];
+        this.sourceFunction = null;
+        this.setParameters = {};
+        this.variables = new Set();
+        this.showGraph = false;
+    }
+
+    public displayTrace(executionId: ExecutionId) {
+        this.currentExecution = executionId;
+        if (this.debugState) {
+            let ds: DebuggerState = this.debugState;
+            //            this.variables = Array.from(ds.sourceVariables.keys());
+            this.debugState!.getTrace(this.currentExecution!).subscribe(
+                (t: Trace) => {
+                    if (t.data.tType === 'line') {
+                        t.data.state.forEach((s: TraceState) => this.variables.add(s.sVariable));
+                    }
+                });
+
+            if (this.graphVariable !== '') {
+                this.SetGraphVariable(this.graphVariable);
+            }
+            ds.getExecution(executionId)
                 .mergeMap((ex: Execution) => {
-					if(ex.data.eType !== 'function') {
-						return Observable.throw(new Error(`DebuggerComponent: cannot display execution traces with type ${ex.data.eType}`));
-					}
-					return Observable.forkJoin(
-						ds.getSourceFunction(ex.data.sFunction)
-                            .mergeMap((sf:SourceFunction) => {
-								this.sourceFunction = sf; return this.fileSystemService.getFileContents(sf.sourcePath)}),
-						Observable.of(ds.getTrace(executionId)));
-				})
+                    if (ex.data.eType !== 'function') {
+                        return Observable.throw(new Error(`DebuggerComponent: cannot display execution traces with type ${ex.data.eType}`));
+                    }
+                    return Observable.forkJoin(
+                        ds.getSourceFunction(ex.data.sFunction)
+                            .mergeMap((sf: SourceFunction) => {
+                                this.sourceFunction = sf; return this.fileSystemService.getFileContents(sf.sourcePath)
+                            }),
+                        Observable.of(ds.getTrace(executionId)));
+                })
                 .mergeMap(([fileContents, traces]) => {
-					this.lines = fileContents.split('\n')
-                        .filter((l,i) => this.sourceFunction && i>=(this.sourceFunction.lineStart-1) && i<(this.sourceFunction.lineStart-1 + this.sourceFunction.lineCount))
-                        .map(l => {return { sourceCode: l, traces: []}});
-					this.lastTraceLine = Number.POSITIVE_INFINITY;
-					this.traceColCount = 0;
-					this.lines.forEach((_, i) => {
-						MatchMaxHeightDirective.markDirty(`debugger-${i}`);
-					});
-					return Observable.from(traces);
-				})
+                    this.lines = fileContents.split('\n')
+                        .filter((l, i) => this.sourceFunction && i >= (this.sourceFunction.lineStart - 1) && i < (this.sourceFunction.lineStart - 1 + this.sourceFunction.lineCount))
+                        .map(l => { return { sourceCode: l, traces: [] } });
+                    this.lastTraceLine = Number.POSITIVE_INFINITY;
+                    this.traceColCount = 0;
+                    this.lines.forEach((_, i) => {
+                        MatchMaxHeightDirective.markDirty(`debugger-${i}`);
+                    });
+                    return Observable.from(traces);
+                })
                 .subscribe({
-					next: (t: Trace)=>{
-						this.addTrace(t);
-					},
-					complete: ()=>{},
-					error: (error:Response)=>{
-						console.error(error);
-					}
-				});
-		}
-		else {
-			console.error('Not attached');
-		}
-	}
+                    next: (t: Trace) => {
+                        this.addTrace(t);
+                    },
+                    complete: () => { },
+                    error: (error: Response) => {
+                        console.error(error);
+                    }
+                });
+        }
+        else {
+            console.error('Not attached');
+        }
+    }
 
-	public addTrace(trace: Trace) {
-		if(trace.data.tType === 'call') {
-			//TODO: properly handle these
-			return;
-		}
-		if(this.sourceFunction) {
-			let line = this.lines[trace.line - this.sourceFunction.lineStart];
-			if(this.lastTraceLine >= trace.line) {
-				this.traceColCount++;
-			}
+    public addTrace(trace: Trace) {
+        if (trace.data.tType === 'call') {
+            //TODO: properly handle these
+            return;
+        }
+        if (this.sourceFunction) {
+            let line = this.lines[trace.line - this.sourceFunction.lineStart];
+            if (this.lastTraceLine >= trace.line) {
+                this.traceColCount++;
+            }
 
-			line.traces[this.traceColCount-1] = trace;
+            line.traces[this.traceColCount - 1] = trace;
 
-			this.lastTraceLine = trace.line;
+            this.lastTraceLine = trace.line;
 
-			MatchMaxHeightDirective.markDirty(`debugger-${trace.line-this.sourceFunction.lineStart}`);
-		}
-	}
+            MatchMaxHeightDirective.markDirty(`debugger-${trace.line - this.sourceFunction.lineStart}`);
+        }
+    }
 
-	public ExecuteFunction() {
-		if(this.debugState && this.sourceFunction) {
-			console.log(this.setParameters);
-			this.debugState.executeFunction(this.sourceFunction.address,this.setParameters)
-                .subscribe((ex:Execution)=>{
-					this.displayTrace(ex.id);
-				}, (e:any) => {
-					console.error(e);
-				});
-		} else {
-			this.snackBar.open('No breakpoint set.', undefined, {
-				duration: 3000
-			});
-		}
-	}
+    public ExecuteFunction() {
+        if (this.debugState && this.sourceFunction) {
+            this.resetGraph();
+            console.log(this.setParameters);
+            this.debugState.executeFunction(this.sourceFunction.address, this.setParameters)
+                .subscribe((ex: Execution) => {
+                    this.displayTrace(ex.id);
+                }, (e: any) => {
+                    console.error(e);
+                });
+        } else {
+            this.snackBar.open('No breakpoint set.', undefined, {
+                duration: 3000
+            });
+        }
+    }
 
-	public GetFunctionAsString():string {
-		if(!this.sourceFunction) {
-			return 'No Function Selected';
-		} else {
-			return this.sourceFunction.name + ' ' + this.sourceFunction.getParametersAsString();
-		}
-	}
+    private resetGraph() {
+        this.showGraph = false;
+        this.graphVariable = "";
+    }
 
-	public GoToFunctionsView() {
-		this.viewService.activeView = 'functions';
-	}
+    public GetFunctionAsString(): string {
+        if (!this.sourceFunction) {
+            return 'No Function Selected';
+        } else {
+            return this.sourceFunction.name + ' ' + this.sourceFunction.getParametersAsString();
+        }
+    }
 
-	public SetGraphVariable(variableName: string): void {
-		if(this.currentExecution !== null) {
-			this.graphData = [];
-			this.graphVariable = variableName;
-			this.lineGraph.onDataUpdated();
+    public GoToFunctionsView() {
+        this.viewService.activeView = 'functions';
+    }
 
-			if(this.debugState && this.currentExecution !== null) {
-				let graphUpdates = Observable.create((observer: Subscriber<Trace>) => {
-					this.debugState!.getTrace(this.currentExecution!).subscribe(
-						(t: Trace) => {
-							if(t.data.tType === 'line') {
-								t.data.state.filter((s:TraceState) => s.sVariable === variableName)
-									.forEach((s:TraceState) => {
-									this.graphData.push({x: this.graphData.length, y: parseInt(s.value)});
-									observer.next();
-								});
-							}
-						},
-						(error:Response)=>{
-							console.error(error);
-						});
-				}).debounceTime(100).subscribe(
-					() => this.lineGraph.onDataUpdated());
-			}
-			else {
-				console.error('Not attached');
-			}
-		}
-	}
+    public SetGraphVariable(variableName: string): void {
+        if (this.currentExecution !== null) {
+            this.graphData = [];
+            this.graphVariable = variableName;
+            this.showGraph = true;
+            this.lineGraph.onDataUpdated();
+
+            if (this.debugState && this.currentExecution !== null) {
+                let graphUpdates = Observable.create((observer: Subscriber<Trace>) => {
+                    this.debugState!.getTrace(this.currentExecution!).subscribe(
+                        (t: Trace) => {
+                            if (t.data.tType === 'line') {
+                                t.data.state.filter((s: TraceState) => s.sVariable === variableName)
+                                    .forEach((s: TraceState) => {
+                                        this.graphData.push({ x: this.graphData.length, y: parseInt(s.value) });
+                                        observer.next();
+                                    });
+                            }
+                        },
+                        (error: Response) => {
+                            console.error(error);
+                        });
+                }).debounceTime(100).subscribe(
+                    () => this.lineGraph.onDataUpdated());
+            }
+            else {
+                console.error('Not attached');
+            }
+        }
+    }
 }
--- client/app/models/DebuggerState.ts
+++ client/app/models/DebuggerState.ts
@@ -10,134 +10,134 @@ import { Trace } from "./Trace";
 
 export class DebuggerState {
 
-	public executions: CacheMap<ExecutionId, Observable<Execution>>;
-	public breakpoints: CacheMap<SourceFunctionId, Observable<Breakpoint>>;
-	public sourceFunctions: CacheMap<SourceFunctionId, Observable<SourceFunction>>;
-	public sourceVariables: CacheMap<string, Observable<SourceVariable>>;
-	public traces: CacheMap<ExecutionId, Observable<Trace>>;
+    public executions: CacheMap<ExecutionId, Observable<Execution>>;
+    public breakpoints: CacheMap<SourceFunctionId, Observable<Breakpoint>>;
+    public sourceFunctions: CacheMap<SourceFunctionId, Observable<SourceFunction>>;
+    public sourceVariables: CacheMap<string, Observable<SourceVariable>>;
+    public traces: CacheMap<ExecutionId, Observable<Trace>>;
 
-	constructor(public info: DebugInfo, protected debuggerHttp: DebuggerHttpService) {
-		this.executions = new CacheMap<ExecutionId, Observable<Execution>>(k => debuggerHttp.getExecution(this.info.id, k));
-		this.breakpoints = new CacheMap<SourceFunctionId, Observable<Breakpoint>>();
-		this.sourceFunctions = new CacheMap<SourceFunctionId, Observable<SourceFunction>>(k => debuggerHttp.getSourceFunction(this.info.id, k));
-		this.sourceVariables = new CacheMap<string, Observable<SourceVariable>>();
-		this.traces = new CacheMap<ExecutionId, Observable<Trace>>(k => debuggerHttp.getTrace(this.info.id, k));
-	}
+    constructor(public info: DebugInfo, protected debuggerHttp: DebuggerHttpService) {
+        this.executions = new CacheMap<ExecutionId, Observable<Execution>>(k => debuggerHttp.getExecution(this.info.id, k));
+        this.breakpoints = new CacheMap<SourceFunctionId, Observable<Breakpoint>>();
+        this.sourceFunctions = new CacheMap<SourceFunctionId, Observable<SourceFunction>>(k => debuggerHttp.getSourceFunction(this.info.id, k));
+        this.sourceVariables = new CacheMap<string, Observable<SourceVariable>>();
+        this.traces = new CacheMap<ExecutionId, Observable<Trace>>(k => debuggerHttp.getTrace(this.info.id, k));
+    }
 
-	public initialize(): Observable<null> {
-		return Observable.forkJoin(
-			//initialize functions
-			this.debuggerHttp.getSourceFunctions(this.info.id)
-				.map((sfs) => {
-					sfs.forEach(sf => this.sourceFunctions.set(sf.address, Observable.of(sf)));
-				}))
-			//initialize breakpoints
-			// this.debuggerHttp.getBreakpoints(this.info.id)
-			// 	.map((bs) => {
-			// 		bs.forEach(b => this.breakpoints.set(b.sFunction.id, Observable.of(b)));
-			// 	}))
-			.map(() => null);
-	}
+    public initialize(): Observable<null> {
+        return Observable.forkJoin(
+            //initialize functions
+            this.debuggerHttp.getSourceFunctions(this.info.id)
+                .map((sfs) => {
+                    sfs.forEach(sf => this.sourceFunctions.set(sf.address, Observable.of(sf)));
+                }))
+            //initialize breakpoints
+            // this.debuggerHttp.getBreakpoints(this.info.id)
+            // 	.map((bs) => {
+            // 		bs.forEach(b => this.breakpoints.set(b.sFunction.id, Observable.of(b)));
+            // 	}))
+            .map(() => null);
+    }
 
-	public getSourceFunction(id: SourceFunctionId): Observable<SourceFunction> {
-		let sf = this.sourceFunctions.get(id);
-		sf.subscribe(() => {}, e => this.sourceFunctions.delete(id));
-		return sf;
-	}
+    public getSourceFunction(id: SourceFunctionId): Observable<SourceFunction> {
+        let sf = this.sourceFunctions.get(id);
+        sf.subscribe(() => { }, e => this.sourceFunctions.delete(id));
+        return sf;
+    }
 
-	public getSourceFunctions(): Observable<{[id: string]: SourceFunction}> {
-		//for now, assume we have all functions, and just flatten them into a map
-		return Observable.forkJoin(Array.from(this.sourceFunctions.keys(), (k: SourceFunctionId) => this.sourceFunctions.get(k)))
-			.switchMap((vals: SourceFunction[]) => {
-				return Observable.of(vals.reduce((o: {[id: string]: SourceFunction}, v: SourceFunction) => { o[v.address] = v; return o;}, {}));
-			});
-	}
+    public getSourceFunctions(): Observable<{ [id: string]: SourceFunction }> {
+        //for now, assume we have all functions, and just flatten them into a map
+        return Observable.forkJoin(Array.from(this.sourceFunctions.keys(), (k: SourceFunctionId) => this.sourceFunctions.get(k)))
+            .switchMap((vals: SourceFunction[]) => {
+                return Observable.of(vals.reduce((o: { [id: string]: SourceFunction }, v: SourceFunction) => { o[v.address] = v; return o; }, {}));
+            });
+    }
 
-	public getSourceVariable(id: string): Observable<SourceVariable> {
-		let sv = this.sourceVariables.get(id);
-		sv.subscribe(() => {}, e => this.sourceVariables.delete(id));
-		return sv;
-	}
+    public getSourceVariable(id: string): Observable<SourceVariable> {
+        let sv = this.sourceVariables.get(id);
+        sv.subscribe(() => { }, e => this.sourceVariables.delete(id));
+        return sv;
+    }
 
-	public getSourceVariables(): Observable<{[id: string]: SourceVariable}> {
-		//for now, assume we have all functions, and just flatten them into a map
-		return Observable.forkJoin(Array.from(this.sourceVariables.keys(), (k: string) => this.sourceVariables.get(k))).defaultIfEmpty([])
-			.switchMap((vals: SourceVariable[]) => {
-				return Observable.of(vals.reduce((o: {[id: string]: SourceVariable}, v: SourceVariable) => { o[v.name] = v; return o;}, {}));
-			});
-	}
+    public getSourceVariables(): Observable<{ [id: string]: SourceVariable }> {
+        //for now, assume we have all functions, and just flatten them into a map
+        return Observable.forkJoin(Array.from(this.sourceVariables.keys(), (k: string) => this.sourceVariables.get(k))).defaultIfEmpty([])
+            .switchMap((vals: SourceVariable[]) => {
+                return Observable.of(vals.reduce((o: { [id: string]: SourceVariable }, v: SourceVariable) => { o[v.name] = v; return o; }, {}));
+            });
+    }
 
-	public getExecution(id: ExecutionId): Observable<Execution> {
-		let ex = this.executions.get(id);
-		ex.subscribe(() => {}, e => this.executions.delete(id));
-		return ex;
-	}
+    public getExecution(id: ExecutionId): Observable<Execution> {
+        let ex = this.executions.get(id);
+        ex.subscribe(() => { }, e => this.executions.delete(id));
+        return ex;
+    }
 
-	public getExecutions(): Observable<{[id: string]: Execution}> {
-		//for now, assume we have all functions, and just flatten them into a map
-		return Observable.forkJoin(Array.from(this.executions.keys(), (k: ExecutionId) => this.executions.get(k))).defaultIfEmpty([])
-			.switchMap((vals: Execution[]) => {
-				return Observable.of(vals.reduce((o: {[id: string]: Execution}, v: Execution) => { o[v.id] = v; return o;}, {}));
-			});
-	}
+    public getExecutions(): Observable<{ [id: string]: Execution }> {
+        //for now, assume we have all functions, and just flatten them into a map
+        return Observable.forkJoin(Array.from(this.executions.keys(), (k: ExecutionId) => this.executions.get(k))).defaultIfEmpty([])
+            .switchMap((vals: Execution[]) => {
+                return Observable.of(vals.reduce((o: { [id: string]: Execution }, v: Execution) => { o[v.id] = v; return o; }, {}));
+            });
+    }
 
-	public executeBinary(args: string, env: string): Observable<Execution> {
-		return this.debuggerHttp.executeBinary(this.info.id, args, env)
-			.map(e => {
-				this.executions.set(e.id, Observable.of(e));
-				return e;
-			});
-	}
+    public executeBinary(args: string, env: string): Observable<Execution> {
+        return this.debuggerHttp.executeBinary(this.info.id, args, env)
+            .map(e => {
+                this.executions.set(e.id, Observable.of(e));
+                return e;
+            });
+    }
 
-	public executeFunction(id: SourceFunctionId, parameters: {[id: string]: any}): Observable<Execution> {
-		return this.debuggerHttp.executeFunction(this.info.id, id, parameters)
-			.map(e => {
-				this.executions.set(e.id, Observable.of(e));
-				return e;
-			});
-	}
+    public executeFunction(id: SourceFunctionId, parameters: { [id: string]: any }): Observable<Execution> {
+        return this.debuggerHttp.executeFunction(this.info.id, id, parameters)
+            .map(e => {
+                this.executions.set(e.id, Observable.of(e));
+                return e;
+            });
+    }
 
-	public stopExecution(id: ExecutionId): Observable<Execution> {
-		return this.debuggerHttp.stopExecution(this.info.id, id)
-			.map(e => {
-				this.executions.set(e.id, Observable.of(e));
-				return e;
-			});
-	}
+    public stopExecution(id: ExecutionId): Observable<Execution> {
+        return this.debuggerHttp.stopExecution(this.info.id, id)
+            .map(e => {
+                this.executions.set(e.id, Observable.of(e));
+                return e;
+            });
+    }
 
-	public getTrace(id: ExecutionId): Observable<Trace> {
-		let t = this.traces.get(id);
-		t.subscribe(() => {}, e => this.traces.delete(id));
-		return t;
-	}
+    public getTrace(id: ExecutionId): Observable<Trace> {
+        let t = this.traces.get(id);
+        t.subscribe(() => { }, e => this.traces.delete(id));
+        return t;
+    }
 
-	public getBreakpoint(id: SourceFunctionId): Observable<Breakpoint> {
-		let b = this.breakpoints.get(id);
-		b.subscribe(() => {}, e => this.breakpoints.delete(id));
-		return b;
-	}
+    public getBreakpoint(id: SourceFunctionId): Observable<Breakpoint> {
+        let b = this.breakpoints.get(id);
+        b.subscribe(() => { }, e => this.breakpoints.delete(id));
+        return b;
+    }
 
-	public getBreakpoints(): Observable<Map<SourceFunctionId, Breakpoint>> {
-		return Observable.forkJoin(Array.from(this.breakpoints.keys(), (k: SourceFunctionId) => this.breakpoints.get(k))).defaultIfEmpty([])
-			.switchMap((vals: Breakpoint[]) => {
-				return Observable.of(vals.reduce((m: Map<SourceFunctionId, Breakpoint>, b: Breakpoint) => { m.set(b.sFunction, b); return m;}, new Map()));
-			});
-	}
+    public getBreakpoints(): Observable<Map<SourceFunctionId, Breakpoint>> {
+        return Observable.forkJoin(Array.from(this.breakpoints.keys(), (k: SourceFunctionId) => this.breakpoints.get(k))).defaultIfEmpty([])
+            .switchMap((vals: Breakpoint[]) => {
+                return Observable.of(vals.reduce((m: Map<SourceFunctionId, Breakpoint>, b: Breakpoint) => { m.set(b.sFunction, b); return m; }, new Map()));
+            });
+    }
 
-	public setBreakpoint(id: SourceFunctionId): Observable<Breakpoint> {
-		return this.debuggerHttp.setBreakpoint(this.info.id, id)
-			.map(b => {
-				this.breakpoints.set(id, Observable.of(b));
-				return b;
-			});
-	}
+    public setBreakpoint(id: SourceFunctionId): Observable<Breakpoint> {
+        return this.debuggerHttp.setBreakpoint(this.info.id, id)
+            .map(b => {
+                this.breakpoints.set(id, Observable.of(b));
+                return b;
+            });
+    }
 
-	public removeBreakpoint(id: SourceFunctionId): Observable<null> {
-		return this.debuggerHttp.removeBreakpoint(this.info.id, id)
-			.map(() => {
-				this.breakpoints.delete(id);
-				return null;
-			});
-	}
+    public removeBreakpoint(id: SourceFunctionId): Observable<null> {
+        return this.debuggerHttp.removeBreakpoint(this.info.id, id)
+            .map(() => {
+                this.breakpoints.delete(id);
+                return null;
+            });
+    }
 
 }
