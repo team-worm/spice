7d84d84fb6726fc591c053fe7da43b638784144f
samdamana <samdamana@gmail.com>
2017-03-06 16:08:49 -0700

    Implemented trace-history list with clickable execution trace objects that replay. Wahoo!

--- client/app/components/debugger/trace-history/trace-history.component.ts
+++ client/app/components/debugger/trace-history/trace-history.component.ts
@@ -2,13 +2,20 @@ import {Component} from "@angular/core";
 import {DebuggerState} from "../../../models/DebuggerState";
 import {MdSidenav} from "@angular/material";
 import {Execution} from "../../../models/Execution";
+import {SourceFunction} from "../../../models/SourceFunction";
+import {ViewService} from "../../../services/view.service";
 
 @Component({
     selector: 'trace-history',
     template: `
 <div class="trace-history-sidenav">
-    <h2>Trace History</h2>
-    <div *ngFor="let e of executions">Execution {{e.id}} : {{e.data.eType}}</div>
+    <h2><md-icon>history</md-icon> Execution Traces</h2>
+    <md-list>
+        <md-list-item *ngFor="let e of executions.reverse()" (click)="ReplayTrace(e)">
+            <md-icon md-list-icon>{{GetListIcon(e)}}</md-icon>
+            <h3 md-line><b>#{{e.ex.id}}</b> : {{GetListTitle(e)}}</h3>
+        </md-list-item>
+    </md-list>
 </div>`,
 })
 
@@ -16,8 +23,9 @@ export class TraceHistoryComponent {
     public debugState:DebuggerState | null = null;
     public sidenav:MdSidenav | null;
 
-    public executions:Execution[] = [];
-    constructor() {
+    public executions:{ex: Execution, func: SourceFunction | null}[] = [];
+
+    constructor(private viewService:ViewService) {
     }
 
     public Toggle() {
@@ -29,7 +37,20 @@ export class TraceHistoryComponent {
 
                 for(let k of this.debugState.executions.keys()) {
                     this.debugState.executions.get(k).subscribe((e:Execution) => {
-                        this.executions.push(e);
+                        if(this.debugState && e.data.eType === "function") {
+                            this.debugState.sourceFunctions.get(e.data.sFunction).subscribe((sf:SourceFunction)=>{
+                                this.executions.push({
+                                    ex: e,
+                                    func: sf
+                                });
+                            })
+                        } else {
+                            this.executions.push({
+                                ex: e,
+                                func: null
+                            });
+                        }
+
                     })
                 }
 
@@ -38,4 +59,29 @@ export class TraceHistoryComponent {
 
         }
     }
+
+    public GetListTitle(e:{ex: Execution, func: SourceFunction | null}):string {
+        if(!!e.func) {
+            return e.func.getAsStringWithParameters();
+        } else {
+            return 'Process'
+        }
+    }
+
+    public GetListIcon(e:{ex: Execution, func: SourceFunction | null}):string {
+        if(e.ex.data.eType === 'function') {
+            return 'library_books'
+        } else {
+            return 'settings'
+        }
+    }
+
+    public ReplayTrace(e:{ex: Execution, func: SourceFunction | null}) {
+        if(this.viewService.debuggerComponent) {
+            this.viewService.debuggerComponent.displayTrace(e.ex.id);
+            this.viewService.activeView = 'debugger';
+        }
+        console.log('Replay Trace', e);
+    }
+
 }
