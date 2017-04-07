e8bb947e053f0c713806be8c4cd9c84e93333368
Samuel Davidson <samdamana@gmail.com>
2017-04-07 01:36:57 -0600

    added type mapping component file. Forgot to include.

---
+++ client/app/components/common/type-mapping.component.ts
@@ -0,0 +1,28 @@
+import {Component, OnInit} from "@angular/core";
+import {SourceFunction} from "../../models/SourceFunction";
+import {MdDialogRef} from "@angular/material";
+@Component({
+    selector: 'spice-type-mapping',
+    template: `        
+        <h2 md-dialog-title><b>{{func.name}}</b> Locals</h2>
+    <md-dialog-content>
+        <div *ngFor="let local of func.locals">
+            {{local.name}}
+        </div>
+    </md-dialog-content>
+    <md-dialog-actions>
+        <button md-raised-button>Save</button>
+        <button md-raised-button md-dialog-close>Cancel</button>
+    </md-dialog-actions>`
+})
+export class TypeMappingComponent implements OnInit{
+
+    public func:SourceFunction;
+
+    constructor(private dialogRef: MdDialogRef<TypeMappingComponent>){
+    }
+
+    ngOnInit() {
+        this.func = <SourceFunction> this.dialogRef.config.data;
+    }
+}
