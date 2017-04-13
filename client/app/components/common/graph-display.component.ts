f14ff27b199650f13e951055dd1b0dac209004c7
Elliot Hatch <elliot.hatch@gmail.com>
2017-04-12 23:17:53 -0600

    Got graph working and more organized

--- client/app/components/common/graph-display.component.ts
+++ client/app/components/common/graph-display.component.ts
@@ -4,7 +4,7 @@ import * as d3 from 'd3';
 
 export type GraphData = {nodes: DataNode[], edges: DataEdge[]};
 export type DataNode = {id: string};
-export type DataEdge = {source: DataNode & d3.SimulationNodeDatum, target: DataNode & d3.SimulationNodeDatum};
+export type DataEdge = {id: number, source: DataNode & d3.SimulationNodeDatum, target: DataNode & d3.SimulationNodeDatum};
 
 @Component({
 	selector: 'spice-graph-display',
@@ -18,8 +18,9 @@ export class GraphDisplayComponent implements OnInit, AfterViewInit, OnChanges, 
 	protected svg: d3.Selection<any,any,any,any>;
 	protected margin: { top: number, right: number, bottom: number, left: number};
 	protected simulation: d3.Simulation<d3.SimulationNodeDatum,d3.SimulationLinkDatum<d3.SimulationNodeDatum>>;
-	protected nodesGroup: any; //svg group
-	protected edgesGroup: any;
+	protected allNodes: d3.Selection<any, any, any, any>;
+	protected allEdges: d3.Selection<any, any, any, any>;
+	protected linkForce: d3.ForceLink<d3.SimulationNodeDatum, d3.SimulationLinkDatum<d3.SimulationNodeDatum>>;
 
 	constructor(protected el: ElementRef) {
 	}
@@ -40,10 +41,10 @@ export class GraphDisplayComponent implements OnInit, AfterViewInit, OnChanges, 
 	ngOnChanges(changes: SimpleChanges) {
 		let nodes = [{id: 'a'},{id: 'b'},{id: 'c'},{id: 'd'}];
 		let edges = [
-			{source: nodes[0], target: nodes[1]},
-			{source: nodes[1], target: nodes[2]},
-			{source: nodes[1], target: nodes[3]},
-			{source: nodes[3], target: nodes[3]}];
+			{id: 0, source: nodes[0], target: nodes[1]},
+			{id: 1, source: nodes[1], target: nodes[2]},
+			{id: 2, source: nodes[1], target: nodes[3]},
+			{id: 3, source: nodes[3], target: nodes[3]}];
 		this.data = {nodes: nodes, edges: edges};
 		this.setup();
 		this.buildSvg();
@@ -88,99 +89,69 @@ export class GraphDisplayComponent implements OnInit, AfterViewInit, OnChanges, 
 				.attr("d", "M0,-5L10,0L0,5");
 
 
-		this.nodesGroup = this.svg.append('g')
-			.attr('class', 'nodes');
+		this.simulation = d3.forceSimulation();
+		this.linkForce = d3.forceLink().id((d:DataNode) => d.id).distance(60);
 
-		this.edgesGroup = this.svg.append('g')
-			.attr('class', 'link');
-
-		this.simulation = d3.forceSimulation()
-			.force("link", d3.forceLink().id((d:DataNode) => d.id).distance(60))
+		this.simulation
+			.force("link",  this.linkForce)
 			.force("charge", d3.forceManyBody()
 								.strength(-120))
-								.force("center", d3.forceCenter(this.width / 2, this.height / 2));
+			.force("center", d3.forceCenter(this.width / 2, this.height / 2));
 	}
 
 	protected populate(): void {
-		this.simulation.nodes(this.data.nodes).on('tick', ticked);
-
-		//this.simulation.force('link')
-			//.links(this.data.edges);
-			//.id(e => d.id);
+		this.simulation.nodes(this.data.nodes).on('tick', () => {this.onSimulationTick();});
+		this.linkForce.links(this.data.edges);
 
 		this.simulation.alphaTarget(0.3).restart();
 
-
-		let edge = this.edgesGroup.selectAll('g')
-			.data(this.data.edges, (e:DataEdge) => `${e.source},${e.target}`);
+		let edge = this.svg.append('g')
+				.attr('class', 'link')
+			.selectAll('g')
+				.data(this.data.edges, (e:DataEdge) => ''+e.id);
 
 		let newEdge = edge.enter().append('g');
-			newEdge.append('line')
-				.attr('marker-end', 'url(#edge)')
-				.attr("stroke", "#aaa")
-				.attr("stroke-width", '1');
+		
+		newEdge.append('line')
+			.attr('marker-end', 'url(#edge)')
+			.attr("stroke", "#aaa")
+			.attr("stroke-width", '1');
 
 		edge.exit().remove();
 
-		let node = this.nodesGroup.selectAll('g')
-			.data(this.data.nodes, (d:DataNode) => d.id);
-
-		node.selectAll('text').attr("fill", '#f00');
-		//node.selectAll('text').attr("fill", (d:DataNode) => {
-			//if(d.explored) {
-				//return _this.color(2);
-			//}
-			//else {
-				//return _this.color(/*d.group*/1); } });
+		let node = this.svg.append('g')
+				.attr('class', 'nodes')
+			.selectAll('g')
+				.data(this.data.nodes, (d:DataNode) => d.id);
 
 		let newNode = node.enter().append('g');
-		newNode.append('text')
-				.text((d:DataNode) => d.id)
-				.attr('fill', '#0f0');
-				
-		//newNode.call(d3.drag()
-					//.on("start", dragstarted)
-					//.on("drag", dragged)
-					//.on("end", dragended));
-
+		
+		newNode.append('circle')
+			.attr('r', 5)
+			.attr('fill', '#4f4');
 		//newNode.append('text')
-			//.text(function(d) { return d.word; })
-			//.attr("fill", function(d) { if(d.explored) { return _this.color(3); } else { return _this.color(/*d.group*/1); } });
+			//.text((d:DataNode) => d.id)
+			//.attr('fill', '#0f0');
+			//.call(d3.drag()
+				//.on("start", dragstarted)
+				//.on("drag", dragged)
+				//.on("end", dragended));
 
 		node.exit().remove();
 
-		let allEdge = edge.merge(newEdge);
-		let allNode = node.merge(newNode);
+		this.allEdges = edge.merge(newEdge);
+		this.allNodes = node.merge(newNode);
+	}
 
-		function ticked() {
-			allEdge.selectAll('line')
-				.attr("x1", (d: DataEdge) => d.source.x)
-				.attr("y1", (d: DataEdge) => d.source.y)
-				.attr("x2", (d: DataEdge) => d.target.x)
-				.attr("y2", (d: DataEdge) => d.target.y)
+	protected onSimulationTick() {
+		this.allEdges.selectAll('line')
+			.attr("x1", (d: DataEdge) => d.source.x!)
+			.attr("y1", (d: DataEdge) => d.source.y!)
+			.attr("x2", (d: DataEdge) => d.target.x!)
+			.attr("y2", (d: DataEdge) => d.target.y!)
 
-			allNode
-				.attr('transform', (d:DataNode & d3.SimulationNodeDatum) => `translate(${d.x},${d.y})`);
-		}
-		//this.svg.append
-		/*
-		this.xScale.domain(d3.extent(this.data, (d: DataXY) => d.x) as [any, any]);
-		this.yScale.domain(d3.extent(this.data, (d: DataXY) => d.y) as [any, any]);
-
-		this.svg.append('path')
-			.attr('class', 'line');
-
-		let path = this.svg.selectAll('path').datum(this.data);
-			path
-			.attr('d', d3.line<DataXY>()
-				.x((d: any) => this.xScale(d.x))
-				.y((d: any) => this.yScale(d.y)));
-
-		this.svg.selectAll('.x.axis')
-			.call(this.xAxis);
-		this.svg.selectAll('.y.axis')
-			.call(this.yAxis);
-		*/
+		this.allNodes
+			.attr('transform', (d:DataNode & d3.SimulationNodeDatum) => `translate(${d.x},${d.y})`);
 	}
 }
 
